86998273a37c75317e785811f17a46ec3a08433a
Kath Pobee-Norris <kpobeenorris@gmail.com>
2018-07-01 13:54:15 +0100

    Refactor form

--- app/helpers/shout_helper.rb
+++ app/helpers/shout_helper.rb
@@ -10,4 +10,12 @@ module ShoutHelper
   def autolink(text)
     text.gsub(/@\w+/) { |dm| link_to dm, user_path(dm[1..-1]) }.html_safe
   end
+
+  def shout_form_for(content_type)
+    form_for Shout.new do |form|
+      form.hidden_field(:content_type, value: content_type) +
+      form.fields_for(:content) { |content_form| yield(content_form) } +
+      form.submit("Shout!")
+    end
+  end
 end
